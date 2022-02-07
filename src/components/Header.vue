13e45f2850cc6351e860190784e5c77b89f1ef6b
MatteoFilippini <matteofilippini8@gmail.com>
2022-02-07 15:00:58 +0100

    added header

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+    <Title />
+  </header>
+</template>
+
+<script>
+import Title from "@/components/Title.vue";
+export default {
+  name: "Header",
+  Title,
+};
+</script>
+
+<style>
+header {
+  background-color: red;
+  height: 50px;
+}
+</style>
