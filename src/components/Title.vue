26886db6abf862d5f40d00b90bde87e11aadb277
MatteoFilippini <matteofilippini8@gmail.com>
2022-02-07 15:01:07 +0100

    added title in header

---
+++ src/components/Title.vue
@@ -0,0 +1,14 @@
+<template>
+  <p>La mia prima app con Vue CLI!</p>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+p {
+  border: 1px solid black;
+  background-color: blue;
+}
+</style>
